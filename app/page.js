0d1c3578f9286662e7ad099bae542c192a84d094
Shiva <shivaiitpatna00@gmail.com>
2026-08-15 14:19:38 +0000

    First Next.js website

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to My First Website</h1>
+      <p>This is my first Next.js site, built step by step.</p>
+    </main>
+  );
+}
